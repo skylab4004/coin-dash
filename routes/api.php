25a6594cfaae01067a26e1c44c76d9eecec1b66d
Skylab4004 <ms8877474@gmail.com>
2021-02-21 01:19:11 +0100

    added PorfolioSnapshotResouce and Collection

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/portolio-snapshot/{id}', function ($id) {
+	return new PortfolioSnapshotResource(PortfolioSnapshot::findOrFail($id));
+});
