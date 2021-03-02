786e4ee967ccf33f08110d4b96d71e10098e1e34
Skylab4004 <ms8877474@gmail.com>
2021-03-02 18:09:32 +0100

    added tailwindcss

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,3 +1,6 @@
 <title>Portafolio</title>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-colorschemes"></script>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
