eeaa338b4cf709ae600389ffcec18afcb3d34257
Skylab4004 <ms8877474@gmail.com>
2021-03-03 00:21:51 +0100

    added force https for non local envs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,28 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		//
+	}
+
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot(UrlGenerator $url) {
+		if (env('APP_ENV') !== 'local') {
+			$url->forceScheme('https');
+		}
+	}
 }
