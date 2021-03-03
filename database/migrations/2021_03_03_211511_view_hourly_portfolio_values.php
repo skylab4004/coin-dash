f0db6bb327f91b67558c36ac24c9a1cfff11eba5
Skylab4004 <ms8877474@gmail.com>
2021-03-03 22:20:51 +0100

    added DailyPortfolioValue and HourlyPortfolioValue models with corresponding migrations with views

---
+++ database/migrations/2021_03_03_211511_view_hourly_portfolio_values.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ViewHourlyPortfolioValues extends Migration {
+
+	public function up() {
+		DB::statement($this->createView());
+	}
+
+	public function down() {
+		DB::statement($this->dropView());
+	}
+
+	private function createView(): string {
+		return <<<SQL
+			CREATE OR REPLACE VIEW hourly_portfolio_values AS (
+			SELECT
+				DATE_FORMAT(FROM_UNIXTIME(snapshot_time/1000), '%Y-%m-%d %H:%i') AS snapshot_time,
+				asset,
+				sum(sum_pln) AS value_in_pln,
+				sum(sum_usd) AS value_in_usd,
+				sum(sum_btc) AS value_in_btc,
+				sum(sum_eth) AS value_in_eth
+			FROM (
+				SELECT
+				snapshot_time,
+				asset,
+				sum(value_in_pln) AS sum_pln,
+				sum(value_in_usd) AS sum_usd,
+				sum(value_in_btc) AS sum_btc,
+				sum(value_in_eth) AS sum_eth
+			FROM portfolio_snapshots  GROUP BY 1, 2
+			UNION
+			SELECT
+				snapshot_time,
+				asset,
+				0, 0, 0, 0
+			FROM
+			(
+				SELECT DISTINCT snapshot_time FROM portfolio_snapshots
+			) AS snapshot_times,
+			(
+				SELECT DISTINCT asset FROM `portfolio_snapshots`) AS assets
+				GROUP BY 1, 2
+			) AS foo
+			WHERE MINUTE(FROM_UNIXTIME(snapshot_time/1000)) = 0
+			GROUP BY 1, 2
+			ORDER BY 1, 2 ASC
+			)
+		SQL;
+	}
+
+	private function dropView(): string {
+		return <<<SQL
+
+            DROP VIEW IF EXISTS `hourly_portfolio_values`;
+            SQL;
+	}
+
+}
