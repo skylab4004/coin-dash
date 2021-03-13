12f24c81d76bec172bd249f35219b60f1d2539fa
Skylab4004 <ms8877474@gmail.com>
2021-03-13 15:29:23 +0100

    added Mexc Api client with method retrieving balances

---
+++ app/Http/Controllers/API/MexcApiClient.php
@@ -0,0 +1,42 @@
+<?php namespace App\Http\Controllers\API;
+
+class MexcApiClient {
+
+	private static $apiUrl = "https://www.mxc.com/";
+
+	private $api;
+
+	public function getBalances() {
+
+		$accountInfoArray = json_decode($this->getAccountInfo(), true);
+		$assets = $accountInfoArray['data'];
+		$balances = [];
+		foreach ($assets as $assetName => $balance) {
+			$assetTotal = $balance['frozen']+$balance['available'];
+			array_push($balances, [$assetName => $assetTotal]);
+		}
+		return $balances;
+	}
+
+	public function getAccountInfo() {
+		$uri = "/open/api/v2/account/info";
+		$req_time = (int) round(microtime(true));
+		$params = ["api_key" => Secret::$MEXC_API_KEY, "req_time" => $req_time];
+		$params["sign"] = self::getSignature("GET", $uri, http_build_query($params));
+		$url = sprintf("%s?%s", self::$apiUrl.$uri, http_build_query($params));
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		$response = curl_exec($curl);
+		curl_close($curl);
+
+		return $response;
+	}
+
+	private static function getSignature($method, $requestUri, $params) {
+		$string = "{$method}\n{$requestUri}\n{$params}";
+		return hash_hmac('sha256', $string, Secret::$MEXC_SECRET_KEY);
+	}
+
+
+}
---
+++ tests/Http/Controllers/API/MexcApiClientTest.php
@@ -0,0 +1,31 @@
+<?php namespace Tests\Http\Controllers\API;
+
+use App\Http\Controllers\API\MexcApiClient;
+use PHPUnit\Framework\TestCase;
+
+class MexcApiClientTest extends TestCase {
+
+	private static $mexcApi;
+
+	public static function setUpBeforeClass(): void {
+		self::$mexcApi = new MexcApiClient();
+	}
+
+	public function testGetBalances() {
+		$balances = $this::$mexcApi->getBalances();
+		print_r($balances);
+		self::assertIsArray($balances);
+	}
+
+	public function testGetAccountInfo() {
+		$accountIn = $this::$mexcApi->getAccountInfo();
+		self::assertJson($accountIn);
+	}
+
+	public function testSignature() {
+		$string = "GET\n/api/v2/order/open_orders\napi_key=mxcV9JCC8iu8zpaiWC&limit=1000&req_time=1572936251&startTime=1572076703000&symbol=MX_ETH&tradeType=BID";
+		$hash = hash_hmac('sha256', $string, "", false);
+		self::assertEquals('0d1a5ee9e76a4907abedc4fe383ddfd0', $hash);
+	}
+
+}
