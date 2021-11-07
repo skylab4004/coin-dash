a1f8d316e2724ad3d3e84a683008d4dd7f4a18f9
Skylab4004 <ms8877474@gmail.com>
2021-11-07 12:52:32 +0100

    jquery imports playaround

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require('laravel-mix');
  */
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/scss/app.scss', 'public/css')
-    .js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps();
+    .js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps()
     .autoload({
             jquery: ['$', 'window.jQuery', 'jQuery'], // more than one
         }
