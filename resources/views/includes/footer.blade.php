1601fb1f257ea53a361a923192272e3321c592ca
Skylab4004 <ms8877474@gmail.com>
2021-12-03 23:49:43 +0100

    render time in footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,13 +1,8 @@
-{{--<div class="bg-gray-900 text-gray-300 mt-3 text-sm">--}}
-{{--    <div>This page took {{ (int) (((microtime(true) - LARAVEL_START))*1000) }} ms to render</div>--}}
-{{--    <div id="copyright text-right">Free software by Skylab4004</div>--}}
-{{--</div>--}}
-
 <!-- Footer -->
 <footer class="sticky-footer">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2021</span>
+            <span>Rendered in {{ (int) (((microtime(true) - LARAVEL_START))*1000) }} ms</span>
         </div>
     </div>
 </footer>
